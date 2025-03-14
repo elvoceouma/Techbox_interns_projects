cc016f8a6b9ea8d796d6566df390a5b72700d520
ZahaEssa <shamoonzahabiy@gmail.com>
2025-03-14 18:49:35 +0300

    Added invoice module

--- swiftstay_workspace/models/swiftstay_rooms.py
+++ swiftstay_workspace/models/swiftstay_rooms.py
@@ -1,11 +1,15 @@
-from odoo import models, fields,api
+from odoo import models, fields, api
+import logging
+
 
 class Rooms(models.Model):
     _name = 'swiftstay.rooms'
     _description = 'Rooms Model'
     _inherit = ['mail.thread']
+    
 
-    name = fields.Char(string='Room Number', required=True, tracking=True)
+    name = fields.Many2one('product.product', string="Room Number", tracking=True)
+
     room_type_id = fields.Many2one('swiftstay.roomtypes', string='Room Type', required=True, tracking=True) 
     num_beds = fields.Integer(string='Number of Beds', required=True, tracking=True)
     floor_number = fields.Integer(string='Floor Number', required=True, tracking=True)
@@ -15,18 +19,21 @@ class Rooms(models.Model):
         ('maintenance', 'Under Maintenance'),
     ], string='Room Status', default='available', required=True, tracking=True)
     price_per_night = fields.Float(related='room_type_id.price_per_night', string="Price Per Night (Ksh.)", store=True, tracking=True)
-    # room_image = fields.Binary(string='Room Image', attachment=True)
-    room_image = fields.Image(
-    string="Room Image",
-    max_width=1024,
-    max_height=1024,
-    store=True,
-    attachment=True, 
-    tracking=True
-)
-    room_colour = fields.Integer(string="Colour", required=True, tracking=True)
+
+    room_image = fields.Binary(
+        string="Room Image",
+        attachment=True,
+        related='name.image_1920', 
+        tracking=True
+    )
     
 
+    room_colour = fields.Integer(string="Colour", required=True, tracking=True)
+
+    @api.depends('name')
+    def compute_display_name(self):
+        for record in self:
+            record.display_name = record.name.name if record.name else "Unnamed Room"
 
     def action_occupied(self):
         for room in self:
@@ -39,3 +46,7 @@ class Rooms(models.Model):
     def action_maintenance(self):
         for room in self:
             room.room_status = 'maintenance'
+    
+ 
+    
+ 
